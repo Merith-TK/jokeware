62737d26033d2ce851a543dcd64a3f393774b897
Merith-TK <merith.tk@gmail.com>
2020-03-18 08:50:42 -0700

    use new library for popup

--- popup/go.mod
+++ popup/go.mod
@@ -2,4 +2,8 @@ module github.com/Merith-TK/jokeware/popup
 
 go 1.14
 
-require github.com/zserge/webview v0.0.0-20200307090906-10ddbc570a0b
+require (
+	github.com/gen2brain/dlgs v0.0.0-20200211102745-b9c2664df42f
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	github.com/zserge/webview v0.0.0-20200307090906-10ddbc570a0b
+)
--- popup/main.go
+++ popup/main.go
@@ -1,13 +1,7 @@
 package main
 
-import "github.com/zserge/webview"
+import "github.com/gen2brain/dlgs"
 
 func main() {
-	debug := true
-	w := webview.New(debug)
-	defer w.Destroy()
-	w.SetTitle("Annoying You")
-	w.SetSize(100, 50, webview.HintNone)
-	w.Navigate("file:///home/merith/Workspace/jokeware/popup/popup.html")
-	w.Run()
+	dlgs.Warning("ERROR", "ERROR")
 }
