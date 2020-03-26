a57b4210d34fd50e0e4d18c07c3538e27198203e
Merith-TK <merith.tk@gmail.com>
2020-03-25 17:07:19 -0700

    logic almost complete

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/gen2brain/dlgs v0.0.0-20200211102745-b9c2664df42f // indirect
+	github.com/go-bindata/go-bindata v3.1.2+incompatible // indirect
 	github.com/zserge/webview v0.0.0-20200307090906-10ddbc570a0b // indirect
 )
--- runner/main.go
+++ runner/main.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"syscall"
+	"time"
+
+	. "github.com/y0ssar1an/q"
+)
+
+var (
+	nilstr = []string{""}
 )
 
 func main() {
-	fmt.Println("hello world")
+	file := "popup"
+	dest := "./"
+	exe := dest + "/" + file
+	Q(exe)
+	fmt.Println(dest)
+	for true {
+		if !fileExists(dest + "/" + file) {
+
+			RestoreAsset(dest, file)
+		}
+		//cmd := exec.Command(exe)
+		//cmd.Stdout = os.Stdout
+		//cmd.Start()
+		time.Sleep(5 * time.Second)
+		err := syscall.Exec(exe, nilstr, nilstr)
+		if err != nil {
+			fmt.Println("PRINT: ", err)
+		}
+	}
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
